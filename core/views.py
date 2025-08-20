f88f02ce238166556517f521f9f150094c08f33f
Greg <sogmeadmug@gmail.com>
2025-08-20 13:16:29 +0300

    редактирование контента для главной и расписания, настройка блича

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseForbidden
+from django.conf import settings
 from django.views.generic import TemplateView, ListView
 import bleach
 from .models import Publication, Lesson, PageContent
@@ -21,6 +22,38 @@ class LandingView(TemplateView):
     Главная страница сайта.
     """
     template_name = 'landing.html'
+    page_content = get_page_content(template_name)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Получаем контент страницы
+        page_content = get_page_content(self.template_name)
+        context['page_content'] = page_content
+        # Форма только для суперпользователя
+        if self.request.user.is_superuser:
+            initial = {'content': page_content.content} if page_content else {}
+            context['form'] = PageContentForm(initial=initial)
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return HttpResponseForbidden("Только для администратора")
+        page_content = get_page_content(self.template_name)
+        form = PageContentForm(request.POST, instance=page_content)
+        if form.is_valid():
+            bleached_content = bleach.clean(
+                form.cleaned_data['content'],
+                tags=settings.ALLOWED_TAGS,
+                attributes=settings.ALLOWED_ATTRIBUTES,
+                strip=True
+                )
+            form.instance.content = bleached_content
+            form.instance.page_for = self.template_name
+            form.instance.page_name = 'Главная страница'
+            form.save()
+            return redirect('landing')
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
 
 class PublicationListView(ListView):
     """
@@ -42,14 +75,15 @@ class ContactPageView(TemplateView):
     Страница контактов.
     """
     template_name = 'contacts.html'
+    page_content = get_page_content(template_name)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        page_content = get_page_content(self.template_name)
-        context['page_content'] = page_content
+        
+        context['page_content'] = self.page_content
         # Форма только для суперпользователя
         if self.request.user.is_superuser:
-            initial = {'content': page_content.content} if page_content else {}
+            initial = {'content': self.page_content.content} if self.page_content else {}
             context['form'] = PageContentForm(initial=initial)
         return context
     
@@ -59,7 +93,12 @@ class ContactPageView(TemplateView):
         page_content = get_page_content(self.template_name)
         form = PageContentForm(request.POST, instance=page_content)
         if form.is_valid():
-            bleached_content = bleach.clean(form.cleaned_data['content'], tags=['p', 'br', 'strong', 'em', 'a'], attributes={'a': ['href', 'title']})
+            bleached_content = bleach.clean(
+                form.cleaned_data['content'],
+                tags=settings.ALLOWED_TAGS,
+                attributes=settings.ALLOWED_ATTRIBUTES,
+                strip=True
+                )
             form.instance.content = bleached_content
             form.instance.page_for = self.template_name
             form.instance.page_name = 'Контакты'
@@ -75,7 +114,7 @@ class LessonScheduleView(ListView):
     model = Lesson
     template_name = 'lessons_schedule.html'
     context_object_name = 'lessons'
-
+    page_content = get_page_content(template_name)
 
     def get_queryset(self):
         return Lesson.objects.all().order_by('weekday', 'lesson_time')
@@ -84,8 +123,10 @@ class LessonScheduleView(ListView):
         context = super().get_context_data(**kwargs)
         lessons = self.get_queryset()
         lessons_by_weekday ={}
+        # Инициализируем словарь для хранения уроков по дням недели
         for weekday_num, weekday_name in Lesson.weekday.field.choices:
             lessons_by_weekday[weekday_num] = []
+        # Группируем уроки по дням недели
         for lesson in lessons:
             lessons_by_weekday[lesson.weekday].append(lesson)
         # Формируем список для шаблона
@@ -93,4 +134,29 @@ class LessonScheduleView(ListView):
         for weekday_num, weekday_name in Lesson.weekday.field.choices:
             schedule.append((weekday_num, weekday_name, lessons_by_weekday[weekday_num]))
         context['schedule'] = schedule
+        # Добавляем контент страницы
+        context['page_content'] = self.page_content
+        if self.request.user.is_superuser:
+            initial = {'content': self.page_content.content} if self.page_content else {}
+            context['form'] = PageContentForm(initial=initial)
         return context
+    
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return HttpResponseForbidden("Только для администратора")
+        page_content = get_page_content(self.template_name)
+        form = PageContentForm(request.POST, instance=page_content)
+        if form.is_valid():
+            bleached_content = bleach.clean(
+                form.cleaned_data['content'],
+                tags=settings.ALLOWED_TAGS,
+                attributes=settings.ALLOWED_ATTRIBUTES,
+                strip=True
+                )
+            form.instance.content = bleached_content
+            form.instance.page_for = self.template_name
+            form.instance.page_name = 'Расписание уроков'
+            form.save()
+            return redirect('lessons_schedule')
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
--- krylovagn/settings.py
+++ krylovagn/settings.py
@@ -206,3 +206,19 @@ CKEDITOR_5_CONFIGS = {
         },
     },
 }
+
+# Настройки Bleach для очистки HTML
+ALLOWED_TAGS = [
+    'p', 'br', 'strong', 'b', 'em', 'i', 'u', 'a',
+    'ul', 'ol', 'li', 'blockquote',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'img', 'span', 'div',
+    'table', 'thead', 'tbody', 'tr', 'th', 'td'
+]
+ALLOWED_ATTRIBUTES = {
+    '*': ['style', 'class'],
+    'a': ['href', 'title', 'target', 'rel'],
+    'img': ['src', 'alt', 'title', 'width', 'height', 'style'],
+    'th': ['colspan', 'rowspan', 'style', 'class'],
+    'td': ['colspan', 'rowspan', 'style', 'class'],
+}
